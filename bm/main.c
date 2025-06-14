813073441dba82b28392dca8f914da654a228f13
Zyphon12342 <shivansh12342@gmail.com>
2025-06-14 20:21:01 +0530

    added cond_jump, halt, debug, program inside BM

--- bm/main.c
+++ bm/main.c
@@ -8,6 +8,7 @@
 #define BM_STACK_CAPACITY 1024
 #define BM_PROGRAM_CAPACITY 1024
 #define ARRAY_SIZE(xs) (sizeof(xs) / sizeof((xs)[0]))
+
 typedef int64_t Word; 
 
 // INST INSTANCE
@@ -18,8 +19,10 @@ typedef enum {
     INST_MULT, 
     INST_DIV, 
     INST_JMP, 
-    INST_JMP_IF, 
+    INST_JMP_IF,
+    INST_EQ, 
     INST_HALT,
+    INST_PRINT_DEBUG, 
 } Inst_Type; 
 
 typedef struct {
@@ -28,7 +31,7 @@ typedef struct {
 } Inst; 
 
 #define MAKE_INST_PUSH(value)   { .type = INST_PUSH, .operand = (value)}
-#define MAKE_INST_JMP(addr)     {.type = INST_JMP, .operand = (addr)}
+#define MAKE_INST_JMP(addr)     { .type = INST_JMP, .operand = (addr)}
 #define MAKE_INST_PLUS          { .type = INST_PLUS}
 #define MAKE_INST_MINUS         { .type = INST_MINUS}
 #define MAKE_INST_MULT          { .type = INST_MULT}
@@ -50,14 +53,17 @@ typedef struct {
 const char *inst_type_as_cstr(Inst_Type type) 
 {
     switch(type) {
-        case INST_PUSH: return "INST_PUSH";
-        case INST_PLUS: return "INST_PLUS";
-        case INST_MINUS: return "INST_MINUS";
-        case INST_MULT: return "INST_MULT";
-        case INST_DIV: return "INST_DIV";
-        case INST_JMP: return "INST_JMP"; 
-        case INST_HALT: return "INST_HALT";
-        default: assert(0 && "inst_type_as_cstr: Unreachable");
+        case INST_PUSH:        return "INST_PUSH";
+        case INST_PLUS:        return "INST_PLUS";
+        case INST_MINUS:       return "INST_MINUS";
+        case INST_MULT:        return "INST_MULT";
+        case INST_DIV:         return "INST_DIV";
+        case INST_JMP:         return "INST_JMP"; 
+        case INST_JMP_IF:      return "INST_JMP_IF"; 
+        case INST_EQ:          return "INST_EQ"; 
+        case INST_HALT:        return "INST_HALT";
+        case INST_PRINT_DEBUG: return "INST_PRINT_DEBUG"; 
+        default:            assert(0 && "inst_type_as_cstr: Unreachable");
     }
 }
 
@@ -74,10 +80,13 @@ typedef enum {
 // BM INSTANCE 
 typedef struct {
     Word stack[BM_STACK_CAPACITY];
+    //current stack size 
     Word stack_size; 
 
     Inst program[BM_PROGRAM_CAPACITY];
+
     Word program_size; 
+    //instruction pointer 
     Word ip; 
 
     int halt; 
@@ -87,13 +96,13 @@ typedef struct {
 const char *Err_as_cstr(Err err) 
 {
     switch(err) {
-        case ERR_OK: return "ERR_OK";
-        case ERR_STACK_OVERFLOW: return "ERR_STACK_OVERFLOW";
-        case ERR_STACK_UNDERFLOW: return "ERR_STACK_UNDERFLOW";
-        case ERR_ILLEGAL_INST: return "ERR_ILLEGAL_INST";
-        case ERR_DIV_BY_ZERO: return "ERR_DIV_BY_ZERO";
+        case ERR_OK:                  return "ERR_OK";
+        case ERR_STACK_OVERFLOW:      return "ERR_STACK_OVERFLOW";
+        case ERR_STACK_UNDERFLOW:     return "ERR_STACK_UNDERFLOW";
+        case ERR_ILLEGAL_INST:        return "ERR_ILLEGAL_INST";
+        case ERR_DIV_BY_ZERO:         return "ERR_DIV_BY_ZERO";
         case ERR_ILLEGAL_INST_ACCESS: return "ERR_ILLEGAL_INST_ACCESS";
-        default: assert(0 && "err_as_cstr: Unreachable");
+        default:                      assert(0 && "err_as_cstr: Unreachable");
     }
 }
 
@@ -162,7 +171,39 @@ Err bm_execute_inst(Bm *bm)
         case INST_HALT: 
             bm->halt = 1; 
             break;
-            
+
+        case INST_EQ:
+            if(bm->stack_size < 2) {
+                return ERR_STACK_UNDERFLOW;
+            }
+            bm->stack[bm->stack_size-2] = (
+                bm->stack[bm->stack_size-2] == bm->stack[bm->stack_size-1]
+            ); 
+            bm->stack_size -= 1; 
+            bm->ip += 1; 
+            break;
+
+        case INST_JMP_IF: 
+            if(bm->stack_size < 1) {
+                return ERR_STACK_UNDERFLOW; 
+            }
+            if(bm->stack[bm->stack_size - 1]) {
+                bm->stack_size -= 1; 
+                bm->ip = inst.operand; 
+            } else {
+                bm->ip += 1; 
+            }
+            break;
+
+        case INST_PRINT_DEBUG: 
+            if(bm->stack_size < 1) {
+                return ERR_STACK_UNDERFLOW; 
+            }
+            fprintf(stdout, "%lld", bm->stack[bm->stack_size - 1]); 
+            bm->stack_size -= 1; 
+            bm->ip += 1; 
+            break; 
+
         default: 
             return ERR_ILLEGAL_INST;
     }
@@ -178,7 +219,7 @@ void bm_dump_stack(FILE* stream, const Bm *bm)
     }
     else{
         for(Word i = 0; i < bm->stack_size; i++) {
-            fprintf(stream, " %lld\n", bm->stack[i]); // lld because word it int64_t 
+            fprintf(stream, " %lld\n", bm->stack[i]); // lld because word is int64_t 
         }
     }
 }
@@ -188,8 +229,7 @@ void bm_dump_stack(FILE* stream, const Bm *bm)
 Inst program[] = {
     MAKE_INST_PUSH(0),  //0
     MAKE_INST_PUSH(1),  //1
-    MAKE_INST_PLUS,     //2
-    MAKE_INST_JMP(1),   //3
+    
 };
 
 // void bm_push_inst(Bm *bm, Inst inst) 
@@ -203,15 +243,17 @@ void bm_load_program_from_memory(Bm *bm, Inst *program, size_t program_size)
 {
     assert(program_size < BM_PROGRAM_CAPACITY);
     memcpy(bm->program, program, sizeof(program[0]) * program_size);  
-    bm->program_size = program_size;
+    bm->program_size = program_size; 
 }  
+
+//INSTANCE OF BM 
 Bm bm = {0};
+
 int main() 
 {
-   
-    bm_dump_stack(stdout, &bm);
-    bm_load_program_from_memory(&bm, program, ARRAY_SIZE(program));
-    for(int i = 0; i < BM_EXECUTION_LIMIT; i++) {
+    bm_load_program_from_memory(&bm, program, ARRAY_SIZE(program)); 
+    bm_dump_stack(stdout, &bm); 
+    for(size_t i = 0; i < BM_EXECUTION_LIMIT && !bm.halt; i++) { 
         Err err = bm_execute_inst(&bm);
         if(err != ERR_OK) {
             fprintf(stderr, "Error: %s\n", Err_as_cstr(err)); 
